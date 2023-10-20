554dd8acfc86429ead74ad1f383a1a37e47ea5c3
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-10-20 09:54:55 -0300

    Criando solução para BuscaLinear.Exercicio01

---
+++ src/03-busca-linear-binaria/BuscaLinear.Exercicio01/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    /*
+     * Escreva um programa que encontre o maior elemento em um array usando busca linear 
+    */
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Maior Elemento no Array *****");
+
+        Console.Write("Informe o tamanho do array: ");
+        int tamanhoDoArray = int.Parse(Console.ReadLine());
+
+        int[] array = new int[tamanhoDoArray];
+        LerArray(array, tamanhoDoArray);
+
+        int maiorElementoArray = EncontrarMaiorElemento(array);
+
+        Console.WriteLine("\n-------- Resultado --------");
+        Console.WriteLine($"O maior elemento do array é: {maiorElementoArray}");
+
+        Console.ReadKey();
+    }
+
+    private static void LerArray (int[] array, int tamanhoDoArray) 
+    {
+        for (int i = 0; i < tamanhoDoArray; i++)
+        {
+            Console.Write($"Elemento [{i}] = ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    private static int EncontrarMaiorElemento(int[] array) 
+    {
+        int maiorElemento = array[0];
+
+        for (int i = 1; i < array.Length; i++) 
+        {
+            if (array[i] > maiorElemento) 
+            {
+                maiorElemento = array[i];
+            }
+        }
+
+        return maiorElemento;
+    }
+}
